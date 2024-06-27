1382b3a70b3b81e101e4876eacb721043cd56e8a
Gabriel <gabriel.coudel-koumba@etu.uca.fr>
2024-06-27 15:43:17 +0200

    :hammer: adding enum class "Category"

---
+++ source/GrindIt.Lib/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace GrindIt.Lib
+{
+    public enum Category
+    {
+        CEREALS = 0,
+        FRUITS = 1,
+        VEGETABLES = 2,
+        DAIRY = 3,
+        MEATS = 4,
+        FISH = 5,
+        FATS = 6,
+        SUGARS = 7,
+        DRINKS = 8,
+    }
+
+    public static class CategoryToString
+    {
+        public static string ToString(Category category) => ((int)category).ToString();
+    }
+}
