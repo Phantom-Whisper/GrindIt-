99e5c0dd12c7728c9f8645540cf21ab2011d9b96
Gabriel <gabriel.coudel-koumba@etu.uca.fr>
2024-06-27 23:41:34 +0200

    start of the exercices handling

---
+++ source/GrindIt.WorkoutLib/Exercices.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GrindIt.WorkoutLib
+{
+    public class Exercices
+    {
+        private string? name;
+
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private readonly ObservableCollection<TargetedMuscles>? targetedMuscles;
+
+        public ReadOnlyObservableCollection<TargetedMuscles> TargetedMuscles
+        {
+            get { return new ReadOnlyObservableCollection<TargetedMuscles>(targetedMuscles); }
+        }
+
+        private bool bodyWeight;
+
+        public bool BodyWeight
+        {
+            get { return bodyWeight; }
+            set { bodyWeight = value; }
+        }
+
+        public Exercices(string name, ObservableCollection<TargetedMuscles> muscles, bool bodyWeight)
+        {
+            if (muscles == null || muscles.Count == 0) throw new ArgumentNullException("The list of targeted muscles must contain at least one muscle.");
+            this.name = name;
+            this.targetedMuscles = muscles;
+            this.BodyWeight = bodyWeight;
+        }
+    }
+}
