3f4d3b13979b4bc7909083308883c6ad308190d4
Phantom-Whisper <gabrielcoudel35@gmail.com>
2025-04-25 17:44:08 +0200

    :construction: interfaces manager & nutrition

---
+++ source/Manager/IManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Manager.IManager;
+
+namespace Manager
+{
+    public interface IManager
+    {
+        public delegate void DelegateDisplayMessage(string message);
+
+        public event DelegateDisplayMessage MessageSended;
+    }
+
+    public class Manager : IManager
+    {
+
+        public event DelegateDisplayMessage? MessageSended;
+
+        public Manager()
+        {
+
+        }
+    }
+}
