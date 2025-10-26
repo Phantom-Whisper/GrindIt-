6b78ab3c2c6d46b19b065389a7d557594e9c2046
Phantom-Whisper <gabrielcoudel35@gmail.com>
2025-10-26 19:13:18 +0100

    :sparkles: added automatic version change in the about section

--- source/GrindIt!/Pages/Account_and_Settings/About.xaml.cs
+++ source/GrindIt!/Pages/Account_and_Settings/About.xaml.cs
@@ -5,8 +5,23 @@ public partial class About
 	public About()
 	{
 		InitializeComponent();
+		
+		GetVersioning();
 	}
 
+	/**
+	 * Get the current versioning off the app
+	 */
+	private void GetVersioning()
+	{
+		var version = AppInfo.Current.Version;
+		
+		AppVersioningLabel.Text = $"Version {version}";
+	}
+
+	/**
+	 * Navigate back to the menu
+	 */
     private void Return_Clicked(object sender, EventArgs e)
     {
 	    Navigation.PopAsync();
